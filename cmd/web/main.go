d189d56b2b2bcd2e35d9e08d2ab905a4494a4827
lowesilvan <lowesilvan@gmail.com>
2024-08-18 15:49:08 +0100

    add static files and handlers

---
+++ cmd/web/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func main() {
+	prometheus.MustRegister(pingCounter)
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.Handle("/metrics", promhttp.Handler())
+
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
